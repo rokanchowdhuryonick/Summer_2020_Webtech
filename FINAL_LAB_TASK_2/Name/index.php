7eef4fbad43bff9026a8b92e2a470b1c4ed09543
Rokan Chowdhury Onick <rokanchowdhury@gmail.com>
2020-08-24 02:21:58 +0600

    init with name file

---
+++ FINAL_LAB_TASK_2/Name/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Name Validation | R</title>
+</head>
+<body>
+	<form method="post" action="" onsubmit="return validateName()">
+		<table>
+			<tr>
+				<td>Name</td>
+				<td><input type="text" name="name" placeholder="Enter your name"></td>
+				<td><input type="submit" name="submit" value="Submit"></td>
+			</tr>
+		</table>
+	</form>
+
+</body>
+</html>
