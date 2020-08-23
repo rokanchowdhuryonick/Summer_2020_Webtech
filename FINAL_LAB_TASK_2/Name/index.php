f21fd6c3e2e02a71fad0a303f10307cfcbc0f9ea
Rokan Chowdhury Onick <rokanchowdhury@gmail.com>
2020-08-24 03:59:01 +0600

    js done

--- FINAL_LAB_TASK_2/Name/index.php
+++ FINAL_LAB_TASK_2/Name/index.php
@@ -2,14 +2,24 @@
 <html>
 <head>
 	<title>Name Validation | R</title>
+	<script type="text/javascript" src="script.js"></script>
 </head>
 <body>
 	<form method="post" action="" onsubmit="return validateName()">
 		<table>
 			<tr>
-				<td>Name</td>
-				<td><input type="text" name="name" placeholder="Enter your name"></td>
-				<td><input type="submit" name="submit" value="Submit"></td>
+				<td>Name : </td>
+				<td><input type="text" name="name" placeholder="Enter your name" id="name"></td>
+				<td><span id="message"></span></td>
+			</tr>
+			<tr>
+				<td></td>
+				<td colspan="2"> <input type="submit" name="submit" value="Submit"></td>
+			</tr>
+			<tr>
+				<td colspan="4">
+					<span id="dump"></span>
+				</td>
 			</tr>
 		</table>
 	</form>
--- FINAL_LAB_TASK_2/Name/script.js
+++ FINAL_LAB_TASK_2/Name/script.js
@@ -1,3 +1,41 @@
 function validateName() {
+	var name = document.getElementById('name').value.trim();
+	document.getElementById('message').style="color:brown";
+	if (name.length<=0) {
+		document.getElementById('message').innerHTML = "*Can not be empty!";
+		return false
+	}else if (name.split(" ").length<2) {
+		document.getElementById('message').innerHTML = "*Contains at least two words!";
+		return false
+	}else if (stringCheck(name.charAt(0))) {
+		if (!checkEveryChar(name)) {
+			document.getElementById('message').innerHTML = "*Can contain a-z or A-Z or dot(.) or dash(-)!";
+			return false
+		}
+	}else{
+		document.getElementById('message').innerHTML = "*Must start with a letter!";
+		return false
+	}
+
+	return true;
+}
+
+function stringCheck(val) {
+  return val.toLowerCase() != val.toUpperCase();
+}
+
+function checkEveryChar(val) {
+	//var datas="";
+	for (var i = 0; i < val.length; i++) {
+		if (stringCheck(val[i])==true || val[i]=='.' || val[i]=='-' || val[i]==' ') {
+			continue;
+		}else{
+			return false;
+		}
+		 //datas+= val[i] + "<br>";
+	}
+
+	//document.getElementById('dump').innerHTML = datas;
 	
+	return true;
 }
