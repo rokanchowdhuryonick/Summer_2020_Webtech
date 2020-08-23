db4e0b24e9446dcb3c659d6d74bbffa2cb3a431f
Rokan Chowdhury Onick <rokanchowdhury@gmail.com>
2020-08-24 05:11:19 +0600

    date of birth php

--- FINAL_LAB_TASK_2/DateOfBirth/index.php
+++ FINAL_LAB_TASK_2/DateOfBirth/index.php
@@ -5,14 +5,18 @@
 	<script type="text/javascript" src="script.js"></script>
 </head>
 <body>
-	
+	<?php
+	if (isset($_POST['submit'])) {
+		echo "Your Submitted Date of Birth is: <b>". $_POST['day']."/".$_POST['month']."/".$_POST['year']."</b>";
+	}
+	?>
 	<form method="post" action="" onsubmit="return validateGender()">
 		<table>
 			<tr>
 				<td>Date of Birth : </td>
 				<td>
-					<input type="text" name="day" placeholder="dd" id="day" size="2">/
-					<input type="text" name="month" placeholder="mm" id="month" size="2">/
+					<input type="text" name="day" placeholder="dd" id="day" size="2"> / 
+					<input type="text" name="month" placeholder="mm" id="month" size="2"> / 
 					<input type="text" name="year" placeholder="yyyy" id="year" size="4">
 				</td>
 				<td><span id="message"></span></td>
@@ -21,11 +25,7 @@
 				<td></td>
 				<td colspan="2"> <input type="submit" name="submit" value="Submit"></td>
 			</tr>
-			<tr>
-				<td colspan="4">
-					<span id="dump"></span>
-				</td>
-			</tr>
+
 		</table>
 	</form>
 
