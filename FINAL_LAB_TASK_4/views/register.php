1e0dfa46edc9a88559fd06deb71a4d0738660c9e
Rokan Chowdhury Onick <rokanchowdhury@gmail.com>
2020-09-01 08:59:04 +0600

    ajax reg

--- FINAL_LAB_TASK_4/views/register.php
+++ FINAL_LAB_TASK_4/views/register.php
@@ -78,7 +78,7 @@
 	 			var xhttp = new XMLHttpRequest();
 	 			xhttp.open('POST', '../php/regCheck.php', true);
 	 			xhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-	 			xhttp.send("fname=Henry&lname=Ford");
+	 			xhttp.send("username="+username+"&password="+password+"&email="+email);
 			    xmlhttp.onreadystatechange = function() {
 			      if (this.readyState == 4 && this.status == 200) {
 			        document.getElementById("txtHint").innerHTML = this.responseText;
