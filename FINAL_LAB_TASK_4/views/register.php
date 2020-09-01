e7d29876affcf83ed657ac57970aa09b740fde3d
Rokan Chowdhury Onick <rokanchowdhury@gmail.com>
2020-09-01 08:57:10 +0600

    ajax init

--- FINAL_LAB_TASK_4/views/register.php
+++ FINAL_LAB_TASK_4/views/register.php
@@ -20,6 +20,7 @@
 	</style>
 </head>
 <body>
+	
 	<table width="100%" border="1" cellpadding="5">
 		<tr>
 			<td>
@@ -28,27 +29,28 @@
 		</tr>
 		<tr height="300px">
 			<td align="center">
+				<div id="message"></div>
 				<form action="../php/regCheck.php" method="post">
 					<fieldset>
 						<legend>SignUp</legend>
 						<table>
 							<tr>
 								<td>Username</td>
-								<td colspan="2"><input type="text" name="username"></td>
+								<td colspan="2"><input type="text" id="username" name="username"></td>
 							</tr>
 							<tr>
 								<td>Email</td>
-								<td><input type="text" name="email" onkeyup="emailCheck(this.value)"></td>
+								<td><input type="text" name="email" id="email" onkeyup="emailCheck(this.value)"></td>
 								<td align="left"><span id="error"></span></td>
 							</tr>
 							<tr>
 								<td>Password</td>
-								<td colspan="2"><input type="password" name="password"></td>
+								<td colspan="2"><input type="password" id="password" name="password"></td>
 							</tr>
 							
 							<tr>
 								<td></td>
-								<td><input type="button" name="submit" value="Submit" onclick="regFormCheck()">  <div id="login"> <a href="login.php">Login</a></div>
+								<td><input type="button" name="submit" id="submit" value="Submit" onclick="regFormCheck()">  <div id="login"> <a href="login.php">Login</a></div>
 								</td>
 
 							</tr>
@@ -65,7 +67,25 @@
 	 	}
 
 	 	function regFormCheck() {
-	 		// body...
+
+	 		var username = document.getElementById('username').value;
+	 		var email = document.getElementById('email').value;
+	 		var password = document.getElementById('password').value;
+	 		if ((username.length<=0) || (email.length<=0) || (password.length<=0)) {
+	 			document.getElementById('message').innerHTML = "Required filed is empty";
+	 		}else{
+	 			document.getElementById('message').innerHTML = "";
+	 			var xhttp = new XMLHttpRequest();
+	 			xhttp.open('POST', '../php/regCheck.php', true);
+	 			xhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+	 			xhttp.send("fname=Henry&lname=Ford");
+			    xmlhttp.onreadystatechange = function() {
+			      if (this.readyState == 4 && this.status == 200) {
+			        document.getElementById("txtHint").innerHTML = this.responseText;
+			      }
+			    };
+	 		}
+
 	 	}
 	 </script>
 </body>
