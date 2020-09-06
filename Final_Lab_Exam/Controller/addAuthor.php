4fc48d09b5f41faaf96e0ec96d2f7cbeb452cf05
Rokan Chowdhury Onick <rokanchowdhury@gmail.com>
2020-09-06 09:13:13 +0600

    author add done

--- Final_Lab_Exam/Controller/addAuthor.php
+++ Final_Lab_Exam/Controller/addAuthor.php
@@ -1,24 +1,28 @@
 <?php
 session_start();
 require_once '../DB/config.php';
+if (!isset($_SESSION['logged_in']) || $_SESSION['logged_in']!=1 || $_SESSION['userType']!="admin") {
+	header('location: ../Views/login.php');
+	exit();
+}
 
 if (isset($_POST['addAuthor'])) {
 	$username = $_POST['username'];
 	$password = $_POST['password'];
 	$name = $_POST['name'];
 	$phone = $_POST['phone'];
-	$userType = 'admin';
-	if (empty($name) || empty($email) || empty($username) || empty($password)) {
+	$userType = 'author';
+	if (empty($name) || empty($phone) || empty($username) || empty($password)) {
 		$_SESSION['message'] = "<font color='brown'>Field can not be empty!</font><br>";
-		header('location:../Views/admins.php');
+		header('location:../Views/addAuthor.php');
 	}else{
-		$q1 = "INSERT INTO users (username, password, email, name, userType) VALUES ('$username','$password','$email','$name','$userType')";
+		$q1 = "INSERT INTO users (name, phone, username, password, userType) VALUES ('$name', '$phone', '$username','$password','$userType')";
 		if (mysqli_query($conn, $q1)) {
-			$_SESSION['message'] = "<font color='darkgreen'>New admin successfully Added!</font><br>";
+			$_SESSION['message'] = "<font color='darkgreen'>New author successfully Added!</font><br>";
 		}else{
 			$_SESSION['message'] = "<font color='brown'>Opps! Error occured with database during insertion!</font><br>";
 			
 		}
-		header('location:../Views/admins.php');
+		header('location:../Views/addAuthor.php');
 	}
 }
--- Final_Lab_Exam/Views/addAuthor.php
+++ Final_Lab_Exam/Views/addAuthor.php
@@ -26,8 +26,39 @@ if (!isset($_SESSION['logged_in']) || $_SESSION['logged_in']!=1 || $_SESSION['us
 		</td>
 	</tr>
 	<tr height="300px">
-		<td>
-			
+		<td align="center">
+			<?php
+			if (isset($_SESSION['message'])) {
+				echo $_SESSION['message'];
+				unset($_SESSION['message']);
+			}
+			?>
+			<form action="../Controller/addAuthor.php" method="post">
+			<table>
+				<tr>
+					<td>Name:</td>
+					<td><input type="text" name="name"></td>
+				</tr>
+				<tr>
+					<td>Phone:</td>
+					<td><input type="text" name="phone"></td>
+				</tr>
+				<tr>
+					<td>UserName:</td>
+					<td><input type="text" name="username"></td>
+				</tr>
+				<tr>
+					<td>Password:</td>
+					<td><input type="text" name="password"></td>
+				</tr>
+				<tr>
+					<td></td>
+					<td>
+						<input type="submit" name="addAuthor" value="Add New Author">
+					</td>
+				</tr>
+			</table>
+			</form>
 		</td>
 	</tr>
 </table>
